23fbe4606f3e3abc5e38fb7774835f2115392f4e
Khalid Hajinur <khalid.hajinur@stscommercial.co.uk>
2018-09-14 13:34:51 +0100

    second commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = "Welcome to laravel";
+        return view('pages.index') ->with('title', $title);
+    }
+
+    public function about(){
+        $title = "About us";
+
+        return view('pages.about') ->with('title', $title);
+    }
+
+    public function services(){
+        $data = array(
+            'title' => 'services',
+            'services'=> ['web design', 'seo', 'app devlopment']
+        );
+        return view('pages.services') ->with($data);
+    }
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,13 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+
+Route::get('/users/{id}', function ($id) {
+    return 'This is '.$id;
+});
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/index','PagesController@index');
+Route::get('/about','PagesController@about');
+Route::get('/services','PagesController@services');
